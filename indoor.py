ed608bcbb4089941c097c8a8ea39da851cc0b0c0
Ismael García <ismagarcia44j@gmail.com>
2026-06-23 18:57:11 -0600

    remove: indoor.py

--- indoor.py
+++
@@ -1,6 +0,0 @@
-#objectives of this program:
-#output the input in lowercase
-#Punctution and whitespaces should be outputted unchanged
-
-text = input("Hi. Type text which you want to convert into lowercase format:")
-print(f"This is your lowercase formatted text: {text.lower ()}")
